5add2950126d3c6eaf125b7d241ac203d25f55e2
Blake Bourque <techplex.engineer@gmail.com>
2021-12-12 09:36:38 -0500

    Capacator init
    npx cap init

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'org.team4909.BionicScout',
+  appName: 'sveltekit-cap',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
