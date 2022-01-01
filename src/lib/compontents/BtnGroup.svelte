1ca03167de16961040a88f00092740319210ff81
Blake Bourque <Techplex.Engineer@gmail.com>
2022-01-01 12:58:37 -0500

    Allow deselect in button groups

--- src/lib/compontents/BtnGroup.svelte
+++ src/lib/compontents/BtnGroup.svelte
@@ -16,12 +16,25 @@
      */
     export let activeIdx = writable(-1);
 
+    export let allowDeselect = true;
+
+    function onClick(newIdx) {
+        return () => {
+            activeIdx.update(prev => {
+                if (allowDeselect && prev == newIdx) {
+                    return -1 //deselect
+                }
+                return newIdx
+            })
+        }
+    }
+
 </script>
 
 <div class="btn-group" role="group">
     {#each options as option, idx}
         <input type="radio" class="btn-check" name={name} id="{name}{idx}" autocomplete="off"
-               checked={$activeIdx === idx} on:click={()=>{activeIdx.set(idx)}}>
+               checked={$activeIdx === idx} on:click={onClick(idx)}>
         <label class="btn btn-outline-primary p-3" for="{name}{idx}">{option.label}</label>
     {/each}
 </div>
