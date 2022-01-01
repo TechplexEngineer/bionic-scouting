8b013938d3f8e8d4d8518530ee1c4efeacfc9380
Blake Bourque <Techplex.Engineer@gmail.com>
2022-01-01 13:01:09 -0500

    Make the home page displays accurate

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,20 +1,31 @@
 <svelte:head>
-	<title>Home</title>
+    <title>Home</title>
 </svelte:head>
 
 <script lang="ts">
-	import { BluetoothSerial } from "bionic-bt-serial";
+    import {BluetoothSerial} from "bionic-bt-serial";
+    import {onMount} from "svelte";
+    import {getDb} from "$lib/store";
+    import {Settings} from "$lib/schema/settings-schema";
 
-	let deviceName = "...";
-	(async () => {
-		deviceName = (await BluetoothSerial.getName()).result;
-	})();
-	let eventName = "Week 0";
+    let deviceName = "...";
+    let eventName = "...";
+    onMount(async () => {
+        deviceName = (await BluetoothSerial.getName()).result;
+        const db = await getDb();
+        db.settings.findOne({selector: {key: Settings.CurrentEvent}}).$.subscribe(v => {
+            // console.log(v);
+            if (v && v.value) {
+                eventName = v.value
+            }
+        })
+    });
+
 </script>
 
 <div class="container-fluid">
-	<h1>This device is <b>{deviceName}</b></h1>
-	<h1>Current Event <b>{eventName}</b></h1>
+    <h1>This device is <b>{deviceName}</b></h1>
+    <h1>Current Event <b>{eventName}</b></h1>
 </div>
 
 <style>
