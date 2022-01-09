d90838c5bb59b3772689c2ad6318d051b9033ab3
Blake Bourque <Techplex.Engineer@gmail.com>
2022-01-09 16:18:31 -0500

    Speedup scout asignments

--- src/routes/tools/setup/_5matchAssiginer.svelte
+++ src/routes/tools/setup/_5matchAssiginer.svelte
@@ -55,7 +55,8 @@
     let matchesPerShift = 5;
 
     async function build() {
-        console.log(JSON.stringify(scoutGroups, null, 4));
+
+        // console.log(JSON.stringify(scoutGroups, null, 4));
 
         const settingEvent = await db.settings.findOne().where({key: Settings.CurrentEvent}).exec();
         if (!settingEvent) {
@@ -73,67 +74,51 @@
         }
         const eventKey = settingEvent.value;
 
-        const matches = await db.matches.find().where({eventKey}).sort({order: "asc"}).exec();
 
-        console.log("Matches: ", matches.map(m => ({k: m.matchKey, o: m.order})));
+        const matches = await db.matches.find().where({eventKey}).sort({order: "asc"}).exec();
+        // console.log("Matches: ", matches.map(m => ({k: m.matchKey, o: m.order})));
+
+        // remove existing scout assignments
+        if (!confirm('WARNING THIS WILL REMOVE MATCH DATA! Proceed?')) {
+            return;
+        }
+        await db.match_metrics.find().where({eventKey}).remove();
+
+
+        let bulkInsertMatches = [];
 
         let numGroups = scoutGroups.length;
         let groupCounter = 0;
         let matchCounter = 0;
         for (const m of matches) {
-            console.log(`G:${groupCounter} M:${matchCounter}`);
+
+            // console.log(`G:${groupCounter} M:${matchCounter}`);
             const redTeams = m.alliances.red.teamKeys.map(t => parseInt(t.replace('frc', '')));
             const blueTeams = m.alliances.blue.teamKeys.map(t => parseInt(t.replace('frc', '')));
 
-
             let scoutCounter = 1;
             for (const t of redTeams) {
-                const query = {
+                bulkInsertMatches.push({
                     eventKey: m.eventKey,
                     matchKey: m.matchKey,
-                    teamNumber: t
-                };
-                const mm = await db.match_metrics.findOne().where(query).exec();
-                if (mm) {
-                    await mm.atomicUpdate(doc => {
-                        doc.scoutName = scoutGroups[groupCounter % numGroups][`scout${scoutCounter++}`].label;
-                        return doc;
-                    });
-                } else {
-                    await db.match_metrics.insert({
-                        eventKey: m.eventKey,
-                        matchKey: m.matchKey,
-                        teamNumber: t,
-                        scoutName: scoutGroups[groupCounter % numGroups][`scout${scoutCounter++}`].label,
-                        createdAt: new Date().getTime(),
-                        updatedAt: new Date().getTime()
-                    });
-                }
+                    teamNumber: t,
+                    scoutName: scoutGroups[groupCounter % numGroups][`scout${scoutCounter++}`].label,
+                    createdAt: new Date().getTime(),
+                    updatedAt: new Date().getTime()
+                });
+
             }
             let blueGroupCounter = groupCounter + 1;
             scoutCounter = 1;
             for (const t of blueTeams) {
-                const query = {
+                bulkInsertMatches.push({
                     eventKey: m.eventKey,
                     matchKey: m.matchKey,
-                    teamNumber: t
-                };
-                const mm = await db.match_metrics.findOne().where(query).exec();
-                if (mm) {
-                    await mm.atomicUpdate(doc => {
-                        doc.scoutName = scoutGroups[blueGroupCounter % numGroups][`scout${scoutCounter++}`].label;
-                        return doc;
-                    });
-                } else {
-                    await db.match_metrics.insert({
-                        eventKey: m.eventKey,
-                        matchKey: m.matchKey,
-                        teamNumber: t,
-                        scoutName: scoutGroups[blueGroupCounter % numGroups][`scout${scoutCounter++}`].label,
-                        createdAt: new Date().getTime(),
-                        updatedAt: new Date().getTime()
-                    });
-                }
+                    teamNumber: t,
+                    scoutName: scoutGroups[blueGroupCounter % numGroups][`scout${scoutCounter++}`].label,
+                    createdAt: new Date().getTime(),
+                    updatedAt: new Date().getTime()
+                });
             }
 
             matchCounter += 1;
@@ -141,15 +126,15 @@
                 groupCounter += 1;
                 console.log(`matchCounter${matchCounter} % matchesPerShift${matchesPerShift} == 0`);
             }
-
         }
+        await db.match_metrics.bulkInsert(bulkInsertMatches)
     }
 
 
 </script>
 
 <h2>5. [Auto] Assign students to matches</h2>
-
+<small>Defaults to first 6 scouts alphabetically</small>
 <table class="table table-striped">
     <thead>
     <tr>
@@ -168,16 +153,19 @@
                 <td>
                     <Select items={scoutsSelect}
                             on:select={assign(idx, 'scout1')}
+                            value={scoutsSelect[idx*3]}
                             containerStyles="--itemPadding:5px"/>
                 </td>
                 <td>
                     <Select items={scoutsSelect}
                             on:select={assign(idx, 'scout2')}
+                            value={scoutsSelect[idx*3+1]}
                             containerStyles="--itemPadding:5px"/>
                 </td>
                 <td>
                     <Select items={scoutsSelect}
                             on:select={assign(idx, 'scout3')}
+                            value={scoutsSelect[idx*3+2]}
                             containerStyles="--itemPadding:5px"/>
                 </td>
                 <td>
