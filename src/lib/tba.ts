bbd50a6bf9a01b2ea7ed8b1f53356cffa3b1eaaa
Blake Bourque <Techplex.Engineer@gmail.com>
2022-01-16 21:10:36 -0500

    Work around TBA bug not supporting if-modified-since header
    https://github.com/the-blue-alliance/the-blue-alliance/issues/4178

--- src/lib/tba.ts
+++ src/lib/tba.ts
@@ -12,15 +12,37 @@ export type Event = { key: string; name: string };
 export type EventList = Event[];
 
 export async function getEvents(year: number): Promise<EventList> {
-	const events = await EventService.getEventsByYearSimple(year);
+	const resp = await fetch(`https://www.thebluealliance.com/api/v3/events/${year}/simple`, {
+		headers: {
+			accept: 'application/json',
+			'x-tba-auth-key': TBA_KEY
+		}
+	});
+	const events = await resp.json();
+
+	// const events = await EventService.getEventsByYearSimple(year);
 
 	return events.map((evt) => ({ key: evt.key, name: evt.name }));
 }
 
 export async function getMatches(eventKey: string): Promise<Array<Match>> {
-	return await EventService.getEventMatches(eventKey);
+	const resp = await fetch(`https://www.thebluealliance.com/api/v3/event/${eventKey}/matches`, {
+		headers: {
+			accept: 'application/json',
+			'x-tba-auth-key': TBA_KEY
+		}
+	});
+	return await resp.json();
+	// return await EventService.getEventMatches(eventKey);
 }
 
 export async function getTeams(eventKey: string): Promise<Array<Team>> {
-	return await EventService.getEventTeams(eventKey);
+	const resp = await fetch(`https://www.thebluealliance.com/api/v3/event/${eventKey}/teams`, {
+		headers: {
+			accept: 'application/json',
+			'x-tba-auth-key': TBA_KEY
+		}
+	});
+	return await resp.json();
+	// return await EventService.getEventTeams(eventKey);
 }
