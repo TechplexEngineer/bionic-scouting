83149497f4fbe7a1ee7506f16c821e40a140e3be
Blake Bourque <techplex.engineer@gmail.com>
2022-02-15 16:53:02 -0500

    Move debounce into utils

--- src/lib/util.ts
+++ src/lib/util.ts
@@ -58,3 +58,19 @@ export const formatDateTime = function (d) {
 	});
 	return dateTimeFormat3.format(date1); //Fri, m/dd
 };
+
+/**
+ * Func is only executed after timeout ms elapse between calls to debounce
+ * @src https://www.freecodecamp.org/news/javascript-debounce-example/
+ * @param func
+ * @param timeout
+ */
+export function debounce(func, timeout = 300) {
+	let timer;
+	return (...args) => {
+		clearTimeout(timer);
+		timer = setTimeout(() => {
+			func.apply(this, args);
+		}, timeout);
+	};
+}
