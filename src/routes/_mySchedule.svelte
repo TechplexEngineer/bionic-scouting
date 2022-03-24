ffb611d32981958760b2d6d319c46e0e3bda7a96
Blake Bourque <Techplex.Engineer@gmail.com>
2022-03-24 16:16:52 -0400

    Make the Lead SS have a nicer view

---
+++ src/routes/_mySchedule.svelte
@@ -0,0 +1,196 @@
+<script lang="ts">
+    import {onMount} from "svelte";
+    import {getDb} from "$lib/store";
+    import {Settings} from "$lib/schema/settings-schema";
+    import {formatDateTime, getOurTeamNumber} from "$lib/util";
+    import {matchSort} from "$lib/matches";
+    import type {RxDocument} from "rxdb";
+    import type {Match} from "$lib/schema/match-schema";
+    import type {SuperScout} from "$lib/schema/super-scout-schema";
+    import type {MatchSubjReport} from "$lib/schema/match-subj-schema";
+
+    export let scout: RxDocument<SuperScout>;
+
+    let ourTeamNumber;
+    let scoutMatches: RxDocument<Match>[] = [];
+
+    function onlyUnique(value, index, self) {
+        return self.indexOf(value) === index;
+    }
+
+    let myTeamMatchesToScout: { team: number, match: string }[] = [];
+    let myAssignedMatches: RxDocument<Match>[] = [];
+
+    let matchReports: RxDocument<MatchSubjReport>[] = [];
+
+
+    onMount(async () => {
+
+        const db = await getDb();
+        let eventName = "...";
+        db.settings.findOne({selector: {key: Settings.CurrentEvent}}).$.subscribe(v => {
+            // console.log(v);
+            if (v && v.value) {
+                eventName = v.value;
+            }
+        });
+
+
+        ourTeamNumber = getOurTeamNumber(db);
+
+
+        let assignedMatchKeys = scout.assignedMatches.map(am => am.assignedMatch)
+        db.matches.find().where({matchKey: {$in: assignedMatchKeys}}).$.subscribe(m => {
+            myAssignedMatches = m.sort(matchSort);
+            // console.log(myAssignedMatches);
+        });
+
+        myTeamMatchesToScout = scout.assignedMatches.map(am => am.teamMatches).flat();
+
+        let myMatches = myTeamMatchesToScout.map(tm => tm.match).flat();
+        myMatches = myMatches.filter(onlyUnique);
+        // console.log("myMatches", myMatches);
+        db.matches.find().where({matchKey: {$in: myMatches}}).$.subscribe(m => {
+            scoutMatches = m.sort(matchSort);
+        });
+
+        db.match_subjective.find().where({eventKey: eventName}).$.subscribe(mr => {
+            matchReports = mr;
+            // console.log("matchReports", matchReports);
+        })
+    });
+
+    const toWatch = function (matchKey: string, teamNumber: number) {
+        return myTeamMatchesToScout.filter(m => m.team == teamNumber && m.match == matchKey).length > 0;
+    };
+    const getPrepFor = (matchKey: string, teamNumber: number) => {
+        for (let m of scout.assignedMatches) {
+            // console.log(m.teamMatches);
+            let filter = m.teamMatches.filter(tm => tm.team == teamNumber && tm.match == matchKey);
+            if (filter.length > 0) {
+                return m.assignedMatch;
+            }
+        }
+        return "???";
+    };
+
+    // Lesson I keep having to relearn, make pure functions, then the reactivity works
+    const isScouted = (matchKey: string, teamNumber: number, matchForKey: string, mr) => {
+        return mr.filter((mr) => {
+            return mr.matchKey == matchKey && mr.teamNumber == teamNumber && mr.matchForKey == matchForKey
+        }).length > 0
+    }
+
+</script>
+
+<h2 class="mt-3">Match Reports / Assigned Matches: <small class="text-muted fs-5">{scout && scout.name}</small>
+</h2>
+<table class="table table-striped">
+    <thead>
+    <tr>
+        <th>Match</th>
+        <th>Date</th>
+        <th colspan="3">Red Alliance</th>
+        <th colspan="3">Blue Alliance</th>
+        <th></th>
+    </tr>
+    </thead>
+    <tbody>
+    {#each myAssignedMatches as m}
+        <tr>
+            <td>
+                <a href="/match/{m.matchKey}">{m.matchKey.toUpperCase()}</a>
+            </td>
+            <td>
+                {formatDateTime(m.scheduledTime)}
+            </td>
+
+            {#each ['red', 'blue'] as color}
+                {#each m.alliances[color].teamKeys as t}
+                    <td class="{color}bg"
+                        class:ourTeam={t.replace('frc','') == ourTeamNumber}
+                        class:toWatch={toWatch(m.matchKey, parseInt(t.replace('frc','')))}>
+                        {t.replace('frc', '')}
+                    </td>
+                {/each}
+            {/each}
+            <td>
+                <a href="match/subjective/report?for={m.matchKey}" class="btn btn-success">Report</a>
+            </td>
+
+
+        </tr>
+    {/each}
+    </tbody>
+</table>
+
+<h2 class="mt-3">My Schedule: <small class="text-muted fs-5">{scout && scout.name} (Super Scout)</small></h2>
+<table class="table table-striped">
+    <thead>
+    <tr>
+        <th>Match</th>
+        <th>Date</th>
+        <th colspan="3">Red Alliance</th>
+        <th colspan="3">Blue Alliance</th>
+    </tr>
+    </thead>
+    <tbody>
+    {#each scoutMatches as m}
+        <tr>
+            <td>
+                <a href="/match/{m.matchKey}">{m.matchKey.toUpperCase()}</a>
+            </td>
+            <td>
+                {formatDateTime(m.scheduledTime)}
+            </td>
+
+            {#each ['red', 'blue'] as color}
+                {#each m.alliances[color].teamKeys as t}
+                    <td class="{color}bg"
+                        class:ourTeam={t.replace('frc','') == ourTeamNumber}
+                        class:toWatch={toWatch(m.matchKey, parseInt(t.replace('frc','')))}>
+                        {t.replace('frc', '')}
+                        {#if toWatch(m.matchKey, parseInt(t.replace('frc', '')))}
+
+                            <a class={isScouted(m.matchKey, parseInt(t.replace('frc', '')), getPrepFor(m.matchKey, parseInt(t.replace('frc', ''))), matchReports) ? "btn btn-success" : "btn btn-warning"}
+                               href={`/match/subjective?match=${m.matchKey}&team=${t.replace('frc','')}&for=${getPrepFor(m.matchKey, parseInt(t.replace('frc', '')))}`}>
+                                {#if isScouted(m.matchKey, parseInt(t.replace('frc', '')), getPrepFor(m.matchKey, parseInt(t.replace('frc', ''))), matchReports) }
+                                    ReScout
+                                {:else}
+                                    Scout
+                                {/if}
+                            </a>
+                            <br>
+                            For: {getPrepFor(m.matchKey, parseInt(t.replace('frc', '')))}
+                        {/if}
+                    </td>
+                {/each}
+            {/each}
+
+
+        </tr>
+    {/each}
+    </tbody>
+</table>
+
+<style>
+    .bluebg {
+        background-color: rgb(201, 218, 248)
+    }
+
+    .ourTeam {
+        filter: brightness(80%);
+    }
+
+    .redbg {
+        background-color: rgb(244, 204, 204)
+    }
+
+    .toWatch.redbg {
+        background-color: #F49090;
+    }
+
+    .toWatch.bluebg {
+        background-color: #7AA8F8;
+    }
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -8,28 +8,17 @@
     import {getDb} from "$lib/store";
     import {Settings} from "$lib/schema/settings-schema";
     import {formatDateTime, getOurTeamNumber} from "$lib/util";
-    import {matchSort} from "$lib/matches";
-    import type {RxDocument} from "rxdb";
-    import type {Match} from "$lib/schema/match-schema";
-    import type {SuperScout} from "$lib/schema/super-scout-schema";
     import {adapterName} from "$lib/bluetooth";
-    import type {MatchSubjReport} from "$lib/schema/match-subj-schema";
+    import MySchedule from "./_mySchedule.svelte";
+    import type {RxDocument} from "rxdb";
+    import type {SuperScout} from "$lib/schema/super-scout-schema";
 
     let deviceName = "...";
     let eventName = "...";
 
     let ourTeamNumber;
-    let scout: RxDocument<SuperScout>;
-    let scoutMatches: RxDocument<Match>[] = [];
 
-    function onlyUnique(value, index, self) {
-        return self.indexOf(value) === index;
-    }
-
-    let myTeamMatchesToScout: { team: number, match: string }[] = [];
-    let myAssignedMatches: RxDocument<Match>[] = [];
-
-    let matchReports: RxDocument<MatchSubjReport>[] = [];
+    let scouts: RxDocument<SuperScout>[] = [];
 
     onMount(async () => {
         deviceName = (await BluetoothSerial.getName()).result;
@@ -49,61 +38,15 @@
         if (first.toUpperCase() !== "SS") {
             return; // nothing to do for non super scouts
         }
-        if (number == "lead") {
+        if (number.toLowerCase() == "lead") {
             // show all
+            scouts = await db.super_scouts.find().where({active: true}).sort({createdAt: "asc"}).exec();
+        } else {
+            scouts = await db.super_scouts.find().where({active: true}).sort({createdAt: "asc"}).skip(number - 1).limit(1).exec();
         }
 
-        //@todo get the correct SS given tablet name
-        scout = await db.super_scouts.findOne().where({active: true}).sort({createdAt: "asc"}).skip(number - 1).exec();
-        if (!scout) {
-            return;
-        }
-
-        let assignedMatchKeys = scout.assignedMatches.map(am => am.assignedMatch)
-        db.matches.find().where({matchKey: {$in: assignedMatchKeys}}).$.subscribe(m => {
-            myAssignedMatches = m.sort(matchSort);
-            // console.log(myAssignedMatches);
-        });
-
-        myTeamMatchesToScout = scout.assignedMatches.map(am => am.teamMatches).flat();
-
-        let myMatches = myTeamMatchesToScout.map(tm => tm.match).flat();
-        myMatches = myMatches.filter(onlyUnique);
-        // console.log("myMatches", myMatches);
-        db.matches.find().where({matchKey: {$in: myMatches}}).$.subscribe(m => {
-            scoutMatches = m.sort(matchSort);
-        });
-
-        db.match_subjective.find().where({eventKey: eventName}).$.subscribe(mr => {
-            matchReports = mr;
-            // console.log("matchReports", matchReports);
-        })
     });
 
-    const toWatch = function (matchKey: string, teamNumber: number) {
-        return myTeamMatchesToScout.filter(m => m.team == teamNumber && m.match == matchKey).length > 0;
-    };
-    const getPrepFor = (matchKey: string, teamNumber: number) => {
-        for (let m of scout.assignedMatches) {
-            // console.log(m.teamMatches);
-            let filter = m.teamMatches.filter(tm => tm.team == teamNumber && tm.match == matchKey);
-            if (filter.length > 0) {
-                return m.assignedMatch;
-            }
-        }
-        return "???";
-    };
-
-    // Lesson I keep having to relearn, make pure functions, then the reactivity works
-    const isScouted = (matchKey: string, teamNumber: number, matchForKey: string, mr) => {
-        return mr.filter((mr) => {
-            // console.log("mk:", mr.matchKey == matchKey, mr.matchKey, matchKey);
-            // console.log("tn:", mr.teamNumber == teamNumber, mr.teamNumber, teamNumber);
-            // console.log("mfk:", mr.matchForKey == matchForKey, mr.matchForKey, matchForKey);
-            // console.log("");
-            return mr.matchKey == matchKey && mr.teamNumber == teamNumber && mr.matchForKey == matchForKey
-        }).length > 0
-    }
 
 </script>
 
@@ -114,120 +57,21 @@
     </div>
 
 
-    {#if $adapterName.startsWith("SS")}
-
-        <h2 class="mt-3">Match Reports / Assigned Matches: <small class="text-muted fs-5">{scout && scout.name}</small>
-        </h2>
-        <table class="table table-striped">
-            <thead>
-            <tr>
-                <th>Match</th>
-                <th>Date</th>
-                <th colspan="3">Red Alliance</th>
-                <th colspan="3">Blue Alliance</th>
-                <th></th>
-            </tr>
-            </thead>
-            <tbody>
-            {#each myAssignedMatches as m}
-                <tr>
-                    <td>
-                        <a href="/match/{m.matchKey}">{m.matchKey.toUpperCase()}</a>
-                    </td>
-                    <td>
-                        {formatDateTime(m.scheduledTime)}
-                    </td>
-
-                    {#each ['red', 'blue'] as color}
-                        {#each m.alliances[color].teamKeys as t}
-                            <td class="{color}bg"
-                                class:ourTeam={t.replace('frc','') == ourTeamNumber}
-                                class:toWatch={toWatch(m.matchKey, parseInt(t.replace('frc','')))}>
-                                {t.replace('frc', '')}
-                            </td>
-                        {/each}
-                    {/each}
-                    <td>
-                        <a href="match/subjective/report?for={m.matchKey}" class="btn btn-success">Report</a>
-                    </td>
-
-
-                </tr>
-            {/each}
-            </tbody>
-        </table>
-
-        <h2 class="mt-3">My Schedule: <small class="text-muted fs-5">{scout && scout.name} (Super Scout)</small></h2>
-        <table class="table table-striped">
-            <thead>
-            <tr>
-                <th>Match</th>
-                <th>Date</th>
-                <th colspan="3">Red Alliance</th>
-                <th colspan="3">Blue Alliance</th>
-            </tr>
-            </thead>
-            <tbody>
-            {#each scoutMatches as m}
-                <tr>
-                    <td>
-                        <a href="/match/{m.matchKey}">{m.matchKey.toUpperCase()}</a>
-                    </td>
-                    <td>
-                        {formatDateTime(m.scheduledTime)}
-                    </td>
-
-                    {#each ['red', 'blue'] as color}
-                        {#each m.alliances[color].teamKeys as t}
-                            <td class="{color}bg"
-                                class:ourTeam={t.replace('frc','') == ourTeamNumber}
-                                class:toWatch={toWatch(m.matchKey, parseInt(t.replace('frc','')))}>
-                                {t.replace('frc', '')}
-                                {#if toWatch(m.matchKey, parseInt(t.replace('frc', '')))}
-
-                                    <a class={isScouted(m.matchKey, parseInt(t.replace('frc', '')), getPrepFor(m.matchKey, parseInt(t.replace('frc', ''))), matchReports) ? "btn btn-success" : "btn btn-warning"}
-                                       href={`/match/subjective?match=${m.matchKey}&team=${t.replace('frc','')}&for=${getPrepFor(m.matchKey, parseInt(t.replace('frc', '')))}`}>
-                                        {#if isScouted(m.matchKey, parseInt(t.replace('frc', '')), getPrepFor(m.matchKey, parseInt(t.replace('frc', ''))), matchReports) }
-                                            ReScout
-                                        {:else}
-                                            Scout
-                                        {/if}
-                                    </a>
-                                    <br>
-                                    For: {getPrepFor(m.matchKey, parseInt(t.replace('frc', '')))}
-                                {/if}
-                            </td>
-                        {/each}
-                    {/each}
-
-
-                </tr>
-            {/each}
-            </tbody>
-        </table>
-
+    {#if $adapterName.toLowerCase().startsWith("ss")}
+        {#if scouts.length > 1}
+            <h3>Super Scouts</h3>
+            <ul>
+                {#each scouts as scout}
+                    <li class="mt-2">Jump To <a href="#{scout.name}">{scout.name}</a></li>
+                {/each}
+            </ul>
+        {/if}
+        {#each scouts as scout}
+            <a id={scout.name}></a>
+            <MySchedule scout={scout}></MySchedule>
+        {/each}
 
     {/if}
 </div>
 
-<style>
-    .bluebg {
-        background-color: rgb(201, 218, 248)
-    }
 
-    .ourTeam {
-        filter: brightness(80%);
-    }
-
-    .redbg {
-        background-color: rgb(244, 204, 204)
-    }
-
-    .toWatch.redbg {
-        background-color: #F49090;
-    }
-
-    .toWatch.bluebg {
-        background-color: #7AA8F8;
-    }
-</style>
