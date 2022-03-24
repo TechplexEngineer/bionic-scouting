9849dd8551245b08752d084e372c6de7255cdb0d
Blake Bourque <Techplex.Engineer@gmail.com>
2022-03-24 16:01:02 -0400

    Cleanup SS page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -108,12 +108,56 @@
 </script>
 
 <div class="container-fluid">
-    <h1>This device is <b>{deviceName}</b></h1>
-    <h1>Current Event <b>{eventName}</b></h1>
+    <div class="d-flex justify-content-around">
+        <h3>This device is <b>{deviceName}</b></h3>
+        <h3>Current Event <b>{eventName}</b></h3>
+    </div>
 
 
     {#if $adapterName.startsWith("SS")}
-        <h2 class="mt-3">My Schedule: <small class="text-muted">{scout && scout.name} (Super Scout)</small></h2>
+
+        <h2 class="mt-3">Match Reports / Assigned Matches: <small class="text-muted fs-5">{scout && scout.name}</small>
+        </h2>
+        <table class="table table-striped">
+            <thead>
+            <tr>
+                <th>Match</th>
+                <th>Date</th>
+                <th colspan="3">Red Alliance</th>
+                <th colspan="3">Blue Alliance</th>
+                <th></th>
+            </tr>
+            </thead>
+            <tbody>
+            {#each myAssignedMatches as m}
+                <tr>
+                    <td>
+                        <a href="/match/{m.matchKey}">{m.matchKey.toUpperCase()}</a>
+                    </td>
+                    <td>
+                        {formatDateTime(m.scheduledTime)}
+                    </td>
+
+                    {#each ['red', 'blue'] as color}
+                        {#each m.alliances[color].teamKeys as t}
+                            <td class="{color}bg"
+                                class:ourTeam={t.replace('frc','') == ourTeamNumber}
+                                class:toWatch={toWatch(m.matchKey, parseInt(t.replace('frc','')))}>
+                                {t.replace('frc', '')}
+                            </td>
+                        {/each}
+                    {/each}
+                    <td>
+                        <a href="match/subjective/report?for={m.matchKey}" class="btn btn-success">Report</a>
+                    </td>
+
+
+                </tr>
+            {/each}
+            </tbody>
+        </table>
+
+        <h2 class="mt-3">My Schedule: <small class="text-muted fs-5">{scout && scout.name} (Super Scout)</small></h2>
         <table class="table table-striped">
             <thead>
             <tr>
@@ -162,45 +206,6 @@
             </tbody>
         </table>
 
-        <h2 class="mt-3">Match Reports / Assigned Matches:</h2>
-        <table class="table table-striped">
-            <thead>
-            <tr>
-                <th>Match</th>
-                <th>Date</th>
-                <th colspan="3">Red Alliance</th>
-                <th colspan="3">Blue Alliance</th>
-                <th></th>
-            </tr>
-            </thead>
-            <tbody>
-            {#each myAssignedMatches as m}
-                <tr>
-                    <td>
-                        <a href="/match/{m.matchKey}">{m.matchKey.toUpperCase()}</a>
-                    </td>
-                    <td>
-                        {formatDateTime(m.scheduledTime)}
-                    </td>
-
-                    {#each ['red', 'blue'] as color}
-                        {#each m.alliances[color].teamKeys as t}
-                            <td class="{color}bg"
-                                class:ourTeam={t.replace('frc','') == ourTeamNumber}
-                                class:toWatch={toWatch(m.matchKey, parseInt(t.replace('frc','')))}>
-                                {t.replace('frc', '')}
-                            </td>
-                        {/each}
-                    {/each}
-                    <td>
-                        <a href="match/subjective/report?for={m.matchKey}" class="btn btn-success">Report</a>
-                    </td>
-
-
-                </tr>
-            {/each}
-            </tbody>
-        </table>
 
     {/if}
 </div>
