3eed086737b7388ab1e120814d56f195b25c7593
Blake Bourque <Techplex.Engineer@gmail.com>
2022-03-31 12:15:27 -0400

    Nasty hacks to get sync to work

--- src/routes/settings.svelte
+++ src/routes/settings.svelte
@@ -10,20 +10,32 @@
     let db: MyDatabase;
     onMount(async () => {
         db = await getDb();
-
-
     })
 
+    let syncMessages = "Sync not started.";
+
     enum SyncDirection {
-        PUSH,
-        PULL,
-        BOTH
+        PUSH = "PUSH",
+        PULL = "PULL",
+        BOTH = "BOTH"
     }
 
+    const LogSyncMessage = (msg: string) => {
+        syncMessages += `${new Date().toLocaleDateString('en-US')} ${new Date().toLocaleTimeString('en-US')}:: ${msg}\n`;
+    }
+
+    //@todo only one sync should happen at a time for messages to make any sense.
     const sync = (syncDir: SyncDirection) => {
         return () => {
+            // this is a total hack
+            process.browser = true;
+            // this solves our sync problem
+
+            syncMessages = '';
+            LogSyncMessage(`Started Sync: Direction ${syncDir}`);
+
             const replicationState = db.pit_scouting.syncCouchDB({
-                remote: "http://tga_app:oH2955UqY8bg@104.248.113.190:5984/event_2022mabos/", // remote database. This can be the serverURL, another RxCollection or a PouchDB-instance
+                remote: "http://tga_app:oH2955UqY8bg@104.248.113.190:5984/events_2022/", // remote database. This can be the serverURL, another RxCollection or a PouchDB-instance
                 waitForLeadership: true,              // (optional) [default=true] to save performance, the sync starts on leader-instance only
                 direction: {                          // direction (optional) to specify sync-directions
                     pull: syncDir == SyncDirection.PULL || syncDir == SyncDirection.BOTH, // default=true
@@ -53,25 +65,31 @@
             replicationState.alive$.subscribe(alive => {
                 console.log("alive", alive);
             });
-            replicationState.complete$.subscribe(completed => {
+            replicationState.complete$.subscribe((completed: boolean | { ok: boolean, errors: string[], last_seq: number, start_time: string, end_time: string, docs_read: number, docs_written: number, doc_write_failures: number, status: string }) => {
                 console.log("completed", completed);
+                if (completed && typeof completed !== "boolean") {
+                    LogSyncMessage(`Sync Complete: read:${completed.docs_read} written:${completed.docs_written}`);
+                }
+                // undo our nasty hack
+                process.browser = undefined;
             });
             replicationState.error$.subscribe(error => {
                 console.log("error", error);
+                LogSyncMessage(`Error Syncing: ${error.message}`);
             });
         }
     }
 </script>
 
 <div class="content">
-    <h1>Settings</h1>
+    <h1>Sync</h1>
     <div class="d-flex justify-content-between mb-4">
         <SpinButton class="btn btn-warning" onClick={sync(SyncDirection.PULL)}>Pull Pit Data</SpinButton>
         <SpinButton class="btn btn-info" onClick={sync(SyncDirection.PUSH)}>Push Pit Data</SpinButton>
     </div>
 
     <label for="syncMessages">Sync Messages:</label>
-    <textarea id="syncMessages" class="form-control mt-2" disabled rows="25"></textarea>
+    <textarea id="syncMessages" class="form-control mt-2" disabled rows="25" bind:value={syncMessages}></textarea>
 </div>
 
 <style>
--- svelte.config.js
+++ svelte.config.js
@@ -3,33 +3,33 @@ import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: preprocess(),
+    // Consult https://github.com/sveltejs/svelte-preprocess
+    // for more information about preprocessors
+    preprocess: preprocess(),
 
-	kit: {
-		adapter: adapter(),
+    kit: {
+        adapter: adapter(),
 
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte',
+        // hydrate the <div id="svelte"> element in src/app.html
+        target: '#svelte',
 
-		vite: {
-			server: {
-				fs: {
-					allow: ['capacitor-plugin-bluteooth-serial']
-				}
-			},
-			resolve: {
-				alias: {
-					events: 'events'
-				}
-			},
-			optimizeDeps: {
-				allowNodeBuiltins: ['pouchdb-browser', 'pouchdb-utils', 'base64id', 'mime-types']
-			}
-		},
-		ssr: false //disable server side rendering
-	}
+        vite: {
+            server: {
+                fs: {
+                    allow: ['capacitor-plugin-bluteooth-serial']
+                }
+            },
+            resolve: {
+                // alias: {
+                //     events: 'events'
+                // }
+            },
+            optimizeDeps: {
+                // allowNodeBuiltins: ['pouchdb-browser', 'pouchdb-utils', 'base64id', 'mime-types']
+            }
+        },
+        ssr: false //disable server side rendering
+    }
 };
 
 export default config;
